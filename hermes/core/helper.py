ced6402969da667e4cf0e3cec90b3b6e7d1cc32f
ryomenhaider <onlinep680@gmail.com>
2026-08-19 17:43:32 +0500

    CRS updated and added a year adjustment fnction

--- hermes/core/helper.py
+++ hermes/core/helper.py
@@ -31,3 +31,22 @@ def check_empty(mode, data, country):
 def empty_result(mode: str):
 
     return np.nan if mode == "F" else pd.Series(dtype=float)
+
+def adjust_year_range(df, year_col, start_year, end_year, fill_method="null", fill_value=0):
+    full_years = pd.DataFrame({year_col: range(start_year, end_year + 1)})
+    
+    df_filtered = df[(df[year_col] >= start_year) & (df[year_col] <= end_year)]
+    
+    adjusted_df = pd.merge(full_years, df_filtered, on=year_col, how='left')
+    
+    if fill_method == "value":
+        adjusted_df = adjusted_df.fillna(fill_value)
+    elif fill_method == "ffill":
+        adjusted_df = adjusted_df.ffill().bfill()
+    elif fill_method == "bfill":
+        adjusted_df = adjusted_df.bfill().ffill()
+    elif fill_method == "linear":
+        adjusted_df = adjusted_df.interpolate(method='linear').bfill().ffill()
+        
+    return adjusted_df
+
